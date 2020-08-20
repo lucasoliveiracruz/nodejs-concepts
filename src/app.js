2f0496efd5daa03a811b4f442e9d437a2137e72f
Locrz <locrz@MBP-de-Danilo-Pela.local>
2020-08-19 23:36:17 -0300

    Node concepts - first challenge completed

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,67 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = { id: uuid(), title, url, techs, likes: 0 };
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: "Repository id is invalid!" });
+  }
+
+  const repositoryIndex = repositories.findIndex((repo) => repo.id === id);
+  const selectedRepository = repositories[repositoryIndex];
+
+  const newRepository = { ...selectedRepository, title, url, techs };
+  repositories[repositoryIndex] = newRepository;
+
+  return response.json(newRepository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: "Repository id is invalid!" });
+  }
+
+  const repositoryIndex = repositories.findIndex((repo) => repo.id === id);
+
+  repositories.splice(repositoryIndex);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: "Repository id is invalid!" });
+  }
+
+  const repositoryIndex = repositories.findIndex((repo) => repo.id === id);
+  const selectedRepository = repositories[repositoryIndex];
+
+  const repository = {
+    ...selectedRepository,
+    likes: selectedRepository.likes + 1,
+  };
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
 });
 
 module.exports = app;
